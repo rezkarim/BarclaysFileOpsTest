06092cc99052495be4a3d3e2de44221b8cfd73e8
md.rezaul.r.karim <md.rezaul.r.karim@CPX-QIV4TSJU02N>
2018-04-12 10:02:21 +0100

    Creating a program to pass in 2 parameters and get version or size of file depending on input

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using ThirdPartyTools;
+using System.Text;
+
 
 namespace FileData
 {
@@ -9,6 +11,27 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+            string functionality, sizeOfFile;
+
+            FileDetails fileDetails = new FileDetails();
+
+            Console.WriteLine("Enter the functionaly command to be performed: ");
+            functionality = Console.ReadLine();
+            Console.WriteLine("Enter the file name/path: ");
+            sizeOfFile = Console.ReadLine();
+
+            if (functionality == "-v" || functionality == "--v" || functionality == "/v" || functionality == "version")
+            {
+                var versionOfFile = fileDetails.Version("C:\test.txt");
+                Console.WriteLine("The current version of your file is: " + versionOfFile);
+            }
+            else if (functionality == "-s" || functionality == "--s" || functionality == "/s" || functionality == "size")
+            {
+                var fileSize = fileDetails.Size("C:\test.txt");
+                Console.WriteLine("Size of your file is: " + fileSize);
+            }
+
+            Console.ReadKey();
         }
     }
 }
